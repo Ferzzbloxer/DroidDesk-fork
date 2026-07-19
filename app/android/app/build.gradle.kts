d7676919946c4fc47a258a21660aeb34838d7056
Ferzzbloxer <ferzzblox@gmail.com>
2026-07-19 02:18:27 -0300

    Change Android namespace for development

--- app/android/app/build.gradle.kts
+++ app/android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.orailnoor.droiddesk"
+    namespace = "com.orailnoor.droiddesk.dev"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
